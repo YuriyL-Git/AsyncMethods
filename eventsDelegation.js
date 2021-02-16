e76157528829117cb599231e7664a865e2d39c36
Yuriy Lipchey <xaosmmm@gmail.com>
2021-02-16 21:05:24 +0200

    capture

--- eventsDelegation.js
+++ eventsDelegation.js
@@ -1,7 +1,10 @@
 const buttons = document.querySelectorAll('button')
 buttons.forEach(button => {
-    button.addEventListener('click', () => console.log('clicked', button.innerHTML))
+    button.addEventListener('click', e => {
+        e.stopPropagation()
+        console.log('clicked', button.innerHTML)
+    })
 })
 
-document.body.addEventListener('click', () => console.log('Body clicked'), {capture: true})
+document.body.addEventListener('click', () => console.log('Body clicked'))
 document.addEventListener('click', () => console.log('Document clicked'))
