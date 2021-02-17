6560099c5002c05acea010120e82e63453908854
Yuriy Lipchey <xaosmmm@gmail.com>
2021-02-17 11:17:10 +0200

    Local and session storages

---
+++ BrowserDataStorage.js
@@ -0,0 +1,20 @@
+//
+
+// Local Storage| Cookies       | Session Storage
+// 10MB         | 4KB           | 5MB
+// Never Expires| Manual        | Expire on tab close
+// Client access| Client/Server | Client access
+// Easy to use  | Hard to use   | Easy
+
+// localStorage.setItem('Name', 'Test value')
+//sessionStorage.setItem('Age', '25')
+
+//localStorage.setItem('Name', 'New test value') // update value
+
+//localStorage.removeItem('Name') //remove items
+//sessionStorage.removeItem('Age')
+
+//console.log(localStorage.getItem('Name'))
+
+
+
